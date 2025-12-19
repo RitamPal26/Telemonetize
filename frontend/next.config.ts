10c830d888aa599dd28f5dc0e0a339a9d2468259
Ritam Pal <ritamjunior26@gmail.com>
2025-12-20 00:37:53 +0530

    fix: CORS fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://telemonetize.onrender.com/api/:path*", // Proxy to Backend
+      },
+      {
+        source: "/socket.io/:path*",
+        destination: "https://telemonetize.onrender.com/socket.io/:path*", // Proxy WebSockets
+      },
+    ];
+  },
 };
 
 export default nextConfig;
